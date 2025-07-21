26da409f6cb91562c0db7720aba6642907b56f53
myles <mnm.fullmetal@gmail.com>
2025-07-21 21:58:54 +1000

    Add user registration functionality and update navigation
    - Implemented user registration view and form.
    - Updated base template to display user greeting and logout option.
    - Added user URLs for registration and login.
    - Enhanced search form in the navigation bar.
    - Updated README with a table of contents.

---
+++ users/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserRegistrationForm(UserCreationForm):
+     class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('email',)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
+def register(request):
+    if request.method =="POST":
+        form = UserCreationForm(request.post)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('pantry:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+        
+    
+
+def toggle_login():
+    pass 
+
