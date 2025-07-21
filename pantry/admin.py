053333934b64fda8b7007d49990595f3a6ed0ef2
myles <mnm.fullmetal@gmail.com>
2025-07-21 23:37:15 +1000

    added ProductAdmin and CustomUserAdmin for managing products and users in the admin interface

--- pantry/admin.py
+++ pantry/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product 
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'code', 'brands') 
+    search_fields = ('product_name', 'code', 'brands') 
+    list_filter = ('brands',) 
+
+
