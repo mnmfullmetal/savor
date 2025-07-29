4f4a6c41c46fbbd7b054ea2bd1f073d14c887bae
mnmfullmetal <mnm.fullmetal@gmail.com>
2025-07-29 18:50:55 +1000

    started to create functionality for adding items to the pantry

--- static/savor.js
+++ static/savor.js
@@ -1,91 +1,103 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const productSearchForm = document.querySelector("#search-form");
 
-document.addEventListener('DOMContentLoaded', () => {
+  productSearchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const barcode = productSearchForm.elements.barcode.value.trim();
+    const productName = productSearchForm.elements.product_name.value.trim();
+    const csrftoken = productSearchForm.elements.csrfmiddlewaretoken.value;
 
-    const productSearchForm = document.querySelector('#search-form')
+    searchProduct(barcode, productName, csrftoken);
+  });
+});
 
-    productSearchForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const barcode = productSearchForm.elements.barcode.value.trim();
-        const productName = productSearchForm.elements.product_name.value.trim();
-        const csrftoken = productSearchForm.elements.csrfmiddlewaretoken.value;
+function searchProduct(barcode = "None", productName = "None", csrftoken) {
+  const productDetailsDiv = document.querySelector("#product_details");
+  productDetailsDiv.innerHTML = "<p>Searching...</p>";
 
-        searchProduct(barcode, productName, csrftoken);
+  if (!barcode && !productName) {
+    alert("Please enter a barcode or product name to search.");
+    productDetailsDiv.innerHTML = "";
+    return;
+  }
 
-    })
+  const requestData = {
+    barcode: barcode,
+    product_name: productName,
+  };
 
-})
+  fetch("/product/search/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Response from Django API:", data);
+      const isAuthenticated = data.is_authenticated;
 
-
-function searchProduct( barcode ='None', productName = 'None', csrftoken) {
-
-    
-    const productDetailsDiv = document.querySelector('#product_details')
-    productDetailsDiv.innerHTML = '<p>Searching...</p>';
- 
-
-    if (!barcode && !productName) {
-        alert("Please enter a barcode or product name to search.");
-        productDetailsDiv.innerHTML = '';
-        return;
-    };
-
-    const requestData = {
-        barcode: barcode,
-        product_name: productName
-    };
-
-    fetch("/product/search/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRFToken": csrftoken
-        },
-        body: JSON.stringify(requestData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Response from Django API:", data);
-        const isAuthenticated = data.is_authenticated;
-
-        if (data.error || data.errors) {
-            const errorMessage = data.error || JSON.parse(data.errors);
-            console.error("Error from backend:", errorMessage);
-            productDetailsDiv.innerHTML = `<p>Error: ${data.error || 'Invalid input.'}</p>`;
-
-        } else if (data.products && data.products.length > 0) {
-            productDetailsDiv.innerHTML = '';
-            data.products.forEach(product => {
-                const productDiv = document.createElement('div');
-                productDiv.innerHTML =
-                    `<h3>${product.product_name || 'No Name'}</h3>
-                     <p>Brands: ${product.brands || 'N/A'}</p>
-                     <p>Code: ${product.code || 'N/A'}</p>
-                     ${product.image_url ? `<img src="${product.image_url}" alt="${product.product_name || 'Product Image'}" style="max-width: 100px; height: auto;">` : ''}
+      if (data.error || data.errors) {
+        const errorMessage = data.error || JSON.parse(data.errors);
+        console.error("Error from backend:", errorMessage);
+        productDetailsDiv.innerHTML = `<p>Error: ${data.error || "Invalid input."
+          }</p>`;
+      } else if (data.products && data.products.length > 0) {
+        productDetailsDiv.innerHTML = "";
+        data.products.forEach((product) => {
+          const productDiv = document.createElement("div");
+          productDiv.innerHTML =
+            `<h3>${product.product_name || "No Name"}</h3>
+                     <p>Brands: ${product.brands || "N/A"}</p>
+                     <p>Code: ${product.code || "N/A"}</p>
+                     ${product.image_url
+              ? `<img src="${product.image_url}" alt="${product.product_name || "Product Image"
+              }" style="max-width: 100px; height: auto;">`
+              : ""
+            }
                      <hr>
-                     <span> <button id="favourite_button" class="btn btn-primary">Like</button> </span> <span> <button id="add_button"  class="btn btn-primary"> Add </button> </span>`;
-                productDetailsDiv.appendChild(productDiv);
-                addButton = document.querySelector("#add_button")
-                addButton.addEventListener("click",() => {
-                    
-                    fetch(`/pantry/add_product`, {
-                        method: "POST", // put? not get because not geeting information 
-                        body: JSON.stringify(requestData)
+                    <div> <input type="number" min="0.01" step="0.01" placeholder="Qty"> <button  class="btn btn-primary add-to-pantry-button" data-product-id=${product.id
+            } data-product-unit="${product.product_quantity_unit}"> Add </button> </div>   
+                    <div><button id="favourite_button" class="btn btn-primary">Favourite</button></div>`;
+          productDetailsDiv.appendChild(productDiv);
 
-                    })
-                    .then(response => json.response)
-                    .then({
+          const addButton = productDiv.querySelector(".add-to-pantry-button");
 
-                    })
-                    // backend will handle user validation and saving to DB
-                })
-            });
+          addButton.addEventListener("click", (event) => {
+            const clickedButton = event.target;
+            const productIdToAdd = clickedButton.data.productId;
+            const productUnitToAdd = clickedButton.data.productUnit;
+            const addProductRequestData = {
+              product_id: productIdToAdd,
+              product_unit: productUnitToAdd 
+            };
 
-        } else {
-            productDetailsDiv.innerHTML = '<p>No products found.</p>';
-        }
+            fetch(`pantry/add_product`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": csrftoken,
+              },
+              body: JSON.stringify(addProductRequestData)
+            })
+              .then((response) => response.json)
+              .catch(error => {
+                console.error("Fetch network error:", error);
+                document.alert(`error: ${error}`)
+              });
+
+          });
+        });
+
+      } else {
+        productDetailsDiv.innerHTML = "<p>No products found.</p>";
+      }
     })
-    .catch(error => {
-        console.error("Fetch network error:", error);
-        productDetailsDiv.innerHTML = '<p>A network error occurred. Please check your connection.</p>';
+    .catch((error) => {
+      console.error("Fetch network error:", error);
+      productDetailsDiv.innerHTML =
+        "<p>A network error occurred. Please check your connection.</p>";
     });
 }
