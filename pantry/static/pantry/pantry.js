c84ac44e9e546c84c17a9f6400cf98fe4181bd32
mnmfullmetal <mnm.fullmetal@gmail.com>
2025-07-31 02:15:42 +1000

    Implement remove pantry item functionality; update JavaScript to handle item removal and adjust quantity, and create corresponding view to process removal requests.

--- pantry/static/pantry/pantry.js
+++ pantry/static/pantry/pantry.js
@@ -2,37 +2,35 @@ document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".remove-button");
 
   if (buttons.length > 0) {
-
     for (const button of buttons) {
       button.addEventListener("click", (event) => {
         const clickedButton = event.target;
-        const productId = clickedButton.dataset.productId;
+        const itemId = clickedButton.dataset.itemId;
+        const csrfToken = clickedButton.dataset.csrfToken;
         const quantityToRemove = clickedButton
           .closest(".pantry-item")
           .querySelector(".remove-quantity-input").value;
 
         const removeRequestData = {
-          productId: productId,
+          itemId: itemId,
           quantityToRemove: quantityToRemove,
+          csrfToken: csrfToken, 
         };
 
-        removeProduct(removeRequestData);
+        removePantryItem(removeRequestData);
       });
-    };
-
-  };
-
+    }
+  }
 });
 
-function removeProduct(removeRequestData) {
+function removePantryItem(removeRequestData) {
+  const csrftoken = removeRequestData.csrfToken;
   fetch(`/pantry/remove_pantryitem`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "X-CSRFToken": csrftoken,
     },
-    body: JSON.stringify(removeRequestData)
-  })
-  .then(response => response.json())
-  
+    body: JSON.stringify(removeRequestData),
+  }).then((response) => response.json());
 }
