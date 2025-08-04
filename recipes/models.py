cc74225ca019f496c379760090fb7654c2f6a849
mnmfullmetal <mnm.fullmetal@gmail.com>
2025-08-04 21:41:02 +1000

    Enhance recipe models and settings: add unique constraint for RecipeIngredient, update Recipe model to include user, and configure environment variables for sensitive settings.

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    pantry_item = models.ForeignKey('pantry.PantryItem', on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ('recipe', 'pantry_item')
+
+    def __str__(self):
+        return f"{self.quantity} {self.unit} of {self.pantry_item.product.product_name} for {self.recipe.title}"
+    
+    
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recipes')
+    title = models.CharField(max_length=255, blank=True, null=True)
+    instructions = models.TextField()
+    ingredients = models.ManyToManyField('pantry.PantryItem', through='RecipeIngredient', related_name='used_in_recipes')
--- savor/settings.py
+++ savor/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
+GOOGLE_API_KEY = env('GOOGLE_API_KEY', default=None)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
