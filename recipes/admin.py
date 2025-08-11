06f71a42f6e74240abfbf1b421e357eff3309633
mnmfullmetal <mnm.fullmetal@gmail.com>
2025-08-11 22:29:32 +1000

    registered recipe models with recipes/admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import SuggestedRecipe, RecipeIngredient
+
+@admin.register(SuggestedRecipe)
+class SuggestedRecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'recipe_owner', 'source', 'generation_date')
+    search_fields = ('title', 'user__username', 'ingredients_list')
+    list_filter = ('source',)
+
+    @admin.display(description='User')
+    def recipe_owner(self, obj):
+        return obj.user.username
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('ingredient_name', 'pantry_item_match', 'suggested_recipe', 'quantity', 'unit')
+    search_fields = ('ingredient_name', 'suggested_recipe__title')
+    list_filter = ('pantry_item_match',)
