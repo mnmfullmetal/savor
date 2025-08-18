bd5a803d9bcf6ef0edf60c644c565021ffe6e335
mnmfullmetal <mnm.fullmetal@gmail.com>
2025-08-19 00:29:56 +1000

    set up celery redbeat for periodic task scheduling

--- savor/settings.py
+++ savor/settings.py
@@ -170,3 +170,7 @@ CACHES = {
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+
+CELERY_BEAT_SCHEDULER = 'redbeat.RedBeatScheduler'
+
+REDBEAT_REDIS_URL = 'redis://127.0.0.1:6379/2'
