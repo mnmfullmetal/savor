18aca3d0fb6c723bc9b21740b51ee72c28941c65
myles <mnm.fullmetal@gmail.com>
2025-10-13 13:57:41 +1100

    implemented middleware that will use the users broswer language setting to translate the static sections of savor when not logged in or the user hasnt set their language preference.
    if the user is logged in and has a language preference, the translations are made in that language they specified.

--- savor/middleware.py
+++ savor/middleware.py
@@ -1,7 +1,8 @@
-# savor/middleware.py
 
 from django.core.cache import cache
 from pantry.signals import schedule_recipe_generation_task 
+from django.utils import translation
+from .utils import LANGUAGE_CODE_MAP 
 
 class PantryRecipeMiddleware:
     def __init__(self, get_response):
@@ -14,6 +15,26 @@ class PantryRecipeMiddleware:
             if not request.session.get('recipes_generated_for_session', False):
                 schedule_recipe_generation_task(user)
                 request.session['recipes_generated_for_session'] = True
+        
+        response = self.get_response(request)
+        return response
+    
+class UserLanguageMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated and hasattr(request.user, 'settings'):
+
+            user_language = request.user.settings.language_preference
+            
+            
+            if user_language:
+                language_code = LANGUAGE_CODE_MAP.get(user_language)
+
+                if language_code:
+                    translation.activate(language_code)
+                    request.session['_language'] = language_code
         
         response = self.get_response(request)
         return response
--- savor/settings.py
+++ savor/settings.py
@@ -62,9 +62,11 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'savor.middleware.UserLanguageMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'savor.middleware.PantryRecipeMiddleware',
